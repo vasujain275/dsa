0bcbbf9379ea6ecf439eb03468e2265a2c943574
Vasu Jain <69643310+vasujain2@users.noreply.github.com>
2023-02-21 23:51:08 +0530

    Day 1 2353hr

--- tuts/001.cpp
+++ tuts/001.cpp
@@ -1,15 +1,11 @@
 #include <iostream>
 using namespace std;
-int main()
-{
-    int a = 5;
-    for (int i = a; i > 0; i--)
-    {
-        for (int j = i; j > 0; j--)
-        {
-            cout<<"*";
-        }
-        cout<<endl;
-    }
+
+int main(){
+    int marks[4] = {99,78,74,36};
+    int* p = marks;
+    cout<<*(p+3);
+
+    
     
 }
