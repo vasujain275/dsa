634a3b61a9e01795fe136abe8ba95b2cb9c08e18
Vasu Jain <69643310+vasujain275@users.noreply.github.com>
2023-05-09 22:52:59 +0530

    Array Practice

---
+++ Arrays/swapAlternateArr.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+void printArr(int arr[],int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout<<arr[i]<<"  ";
+    }
+}
+
+void swapArray(int arr[],int size)
+{
+    int a;
+    for (int i = 0; i < size-1;)
+    {
+        a=arr[i];
+        arr[i]=arr[i+1];
+        arr[i+1]=a;
+        i=i+2;
+    }
+    
+}
+
+int main(){
+    int arr[5];
+    cout<<"Enter the Values for Array: ";
+    for (int i = 0; i < 5; i++)
+    {
+        cin>>arr[i];
+    }
+    swapArray(arr,5);
+    printArr(arr,5);
+    return 0;
+}
