3cfc9c6f4e24ec032d6e42356beccf8fd4d5322d
vasujain275 <vasujain275@gmail.com>
2024-07-31 19:14:57 +0530

    grokking: ch01 refactor

--- grokking-algorithms/ch01/binary-search.js
+++ grokking-algorithms/ch01/binary-search.js
@@ -1,16 +1,16 @@
 // Array to perform binary search in
-arr = [1, 3, 5, 6, 8, 11, 34, 56, 78, 99];
+let arr = [1, 3, 5, 6, 8, 11, 34, 56, 78, 99];
 
 // Key to search in the Array
-item = 78;
+let item = 78;
 
 // Binary Search Logic
 
-low = 0;
-high = arr.length - 1;
+let low = 0;
+let high = arr.length - 1;
 
 while (low <= high) {
-  mid = Math.floor((low + high) / 2);
+  let mid = Math.floor((low + high) / 2);
 
   if (arr[mid] === item) {
     console.log(`The item is at index ${mid}`);
